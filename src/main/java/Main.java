965ef954361ff909e6e688178dd2a4f74f78c745
hubert <hubert.mazur@vp.pl>
2019-06-17 22:09:03 +0200

    Add main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,31 @@
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadz treść smsa:");
+        String textMessage = scanner.nextLine();
+        System.out.println("Wprowadz maksymalną długlość smsa:");
+        int lenght = scanner.nextInt();
+        System.out.println("Podaj koszt jednego smsa w PLN-ach: ");
+        BigDecimal costOfOneSms = scanner.nextBigDecimal();
 
+        NaiveCompressor naiveCompressor = new NaiveCompressor();
+        Paginator paginator = new Paginator(lenght);
+        List<String> dividedSMS = paginator.paginate(naiveCompressor.compress(textMessage));
+        int amountOfSMS = dividedSMS.size();
+
+
+        CostCalculator costCalculator = new CostCalculator(costOfOneSms);
+        BigDecimal totalCost = costCalculator.calculate(amountOfSMS) ;
+
+        System.out.println(
+                String.format("Twoj sms zostanie wysłany w %d wiadomościach," +
+                        " całkowity koszt wysłania to %.2f zł. Lista wiadomości: %s", amountOfSMS,totalCost, dividedSMS)
+        );
 
     }
 }
